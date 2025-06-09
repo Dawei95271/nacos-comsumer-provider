7c0e080487b8c274c29ba7bdf85402a44ef2a52b
dawei <dawei95271@qq.com>
2025-06-09 19:58:31 +0800

    first commit

---
+++ nacos-discovery-consumer/src/main/java/com/alibaba/nacos/DiscoveryConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.alibaba.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+// 开启服务注册功能
+@EnableDiscoveryClient
+public class DiscoveryConsumerApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryConsumerApplication.class, args);
+    }
+ 
+}
---
+++ nacos-discovery-provider/src/main/java/com/alibaba/nacos/DiscoveryProviderApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DiscoveryProviderApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryProviderApplication.class, args);
+    }
+ 
+}
